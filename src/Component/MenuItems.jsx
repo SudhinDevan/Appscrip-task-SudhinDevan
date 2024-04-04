4c633e65bc7a37d27f28547fa619c178a4b729d3
Sudhin Dev <135108500+SudhinDevan@users.noreply.github.com>
2024-04-04 17:07:51 +0530

    first commit

---
+++ src/Component/MenuItems.jsx
@@ -0,0 +1,15 @@
+const MenuItems = () => {
+  return (
+    <>
+      <div className="font-semibold text-xl flex justify-center p-5 gap-12">
+        <h1>SHOP</h1>
+        <h1>SKILLS</h1>
+        <h1>STORIES</h1>
+        <h1>ABOUT</h1>
+        <h1>CONTACT US</h1>
+      </div>
+    </>
+  );
+};
+
+export default MenuItems;
---
+++ src/Component/ProductCard.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+
+const ProductCard = (props) => {
+  const [liked, setLiked] = useState(true);
+
+  const handleLike = () => {
+    setLiked(!liked);
+  };
+
+  return (
+    <>
+      <div className="w-72 h-[350px] m-5 flex flex-col items-center">
+        <div>
+          <img
+            src={props.productImage}
+            alt="Product Image"
+            className="w-60 h-72 object-contain"
+          />
+        </div>
+        <div className="justify-center w-full">
+          <h1 className="font-semibold mt-2 text-sm text-gray-700 truncate">
+            {props.productName}
+          </h1>
+          <div className="flex justify-between">
+            <h1 className="text-gray-400 text-xs">
+              <span className="underline">Signin</span> or create an account to
+              see pricing
+            </h1>
+            {liked ? (
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                onClick={handleLike}
+              >
+                <path
+                  d="M12.62 20.8097C12.28 20.9297 11.72 20.9297 11.38 20.8097C8.48 19.8197 2 15.6897 2 8.68973C2 5.59973 4.49 3.09973 7.56 3.09973C9.38 3.09973 10.99 3.97973 12 5.33973C13.01 3.97973 14.63 3.09973 16.44 3.09973C19.51 3.09973 22 5.59973 22 8.68973C22 15.6897 15.52 19.8197 12.62 20.8097Z"
+                  stroke="#292D32"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            ) : (
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 20 18"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                onClick={handleLike}
+              >
+                <path
+                  d="M10.62 17.8096C10.28 17.9296 9.72 17.9296 9.38 17.8096C6.48 16.8196 0 12.6896 0 5.68961C0 2.59961 2.49 0.0996094 5.56 0.0996094C7.38 0.0996094 8.99 0.97961 10 2.33961C11.01 0.97961 12.63 0.0996094 14.44 0.0996094C17.51 0.0996094 20 2.59961 20 5.68961C20 12.6896 13.52 16.8196 10.62 17.8096Z"
+                  fill="#EB4C6B"
+                />
+              </svg>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProductCard;
